e1391d06e6831cf756f3873aa45aeff6ab8c8552
Michael Sarahan <msarahan@gmail.com>
2016-05-11 15:12:40 -0500

    move VS activation line to bottom, so that it doesn't get overridden by other env vars

--- conda_build/windows.py
+++ conda_build/windows.py
@@ -196,14 +196,14 @@ def build(m, bld_bat):
         with open(bld_bat) as fi:
             data = fi.read()
         with open(join(src_dir, 'bld.bat'), 'w') as fo:
-            fo.write(msvc_env_cmd(bits=cc.bits, override=m.get_value('build/msvc_compiler', None)))
-            fo.write('\n')
             # more debuggable with echo on
             fo.write('@echo on\n')
             for key, value in env.items():
                 fo.write('set "{key}={value}"\n'.format(key=key, value=value))
             fo.write("set INCLUDE={};%INCLUDE%\n".format(env["LIBRARY_INC"]))
             fo.write("set LIB={};%LIB%\n".format(env["LIBRARY_LIB"]))
+            fo.write(msvc_env_cmd(bits=cc.bits, override=m.get_value('build/msvc_compiler', None)))
+            fo.write('\n')
             fo.write("REM ===== end generated header =====\n")
             fo.write(data)
 
